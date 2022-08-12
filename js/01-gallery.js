6978b8b9a1142004821141cc5429eefcbb0feca7
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-12 19:58:26 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,32 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const div = document.querySelector('.gallery');
 
-
-
-const makeGalleryItems = (images)  => 
+const renderGalleryItems = (images)  => 
     images.reduce(
         (acc, { preview, original, description }) => acc + ` 
     <div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href='${original}'>
     <img class="gallery__image" src=${preview} data-source=${original} alt=${description} />
         </a>
         </div>`, ''
     );
 
-
-const result = makeGalleryItems(galleryItems);
+const result = renderGalleryItems(galleryItems);
 div.insertAdjacentHTML('beforeend', result);
 
 
-// console.log(makeGalleryItems);
+const clickEvent = (event) => {
+    event.preventDefault();
+    if(event.target.classList.contains("gallery"))
+    return;
 
+    const sourceImg = event.target.dataset.source;
 
+    const showImages = basicLightbox.create(`
+    <img src="${sourceImg}"width="800" height="600">`, {
+    onShow: () => {
+        window.addEventListener("keydown", closeImages);
+    },
+    onClose: () => {
+        window.removeEventListener("keydown", closeImages);
+    },
+    });
+    function closeImages (event) {
+    if (event.code === 'Escape') {
+        showImages.close();
+    }
+    }
+    showImages.show();
+} 
 
-// const container = document.querySelector('.gallery');
+div.addEventListener("click",clickEvent);
 
-// container.addEventListener('click', onClick);
-// function onClick(e) {
-//     console.log(e.target.textContent);
-// };
-
-console.log(galleryItems);
