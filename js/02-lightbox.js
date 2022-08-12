db42499eb281ec2232f4c93596e178208ffc17d2
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-12 20:40:15 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const div = document.querySelector('.gallery');
+
+const renderGalleryItems = (images)  => 
+    images.reduce(
+        (acc, { preview, original, description }) => acc + ` 
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, ''
+    );
+
+const result = renderGalleryItems(galleryItems);
+div.insertAdjacentHTML('beforeend', result);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: '250',
+});
 
 console.log(galleryItems);
